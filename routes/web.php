d7ba9fa3a8adfb34ea095ee10ce0d971edb63cb7
陳炫樺 <chenxuanhua456@gmail.com>
2025-01-02 09:30:30 +0800

    產生controller 設定Home路由

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//    Route::get('/', function () {
+//        return view('welcome');
+//});
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\NewsController;
+
+Route::get('/',[HomeController::class,'index'])->name('home.index');
+Route::get('/', function () {return view('home');});
