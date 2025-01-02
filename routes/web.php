b171ed7880742102e5e127bf268ec209169d2e90
陳炫樺 <chenxuanhua456@gmail.com>
2025-01-02 09:32:48 +0800

    設定About路由

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,6 @@ use App\Http\Controllers\NewsController;
 
 Route::get('/',[HomeController::class,'index'])->name('home.index');
 Route::get('/', function () {return view('home');});
+
+Route::get('/about',[AboutController::class,'index'])->name('about.index');
+Route::get('/about', function () {return view('about');});
